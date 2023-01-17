95fd45cf0e24ff094960cea5eb57ea9d379410d1
Salem Shaikh <slmsshk21@gmail.com>
2023-01-17 15:14:41 +0530

    Update main.py

--- Chatbot/main.py
+++ Chatbot/main.py
@@ -3,18 +3,21 @@ import streamlit as st
 st.title('ChatBot')
 
 import openai
-openai.api_key = "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"
+
+openai.api_key = "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"
+
 
 prompt = (f"Write a story about an adventure")
 text_input=st.text_input('Enter a prompt')
 t=st.slider('slide to set temperature value', min_value=0.1, max_value=1.0)
+
 completions = openai.Completion.create(
     engine="text-davinci-002",
     prompt=text_input,
-    max_tokens=float('-inf'),
+    max_tokens=2000,
     n=1,
     stop=None,
-    temperature=0.5,
+    temperature=t,
 )
 
 
