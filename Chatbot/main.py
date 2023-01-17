1005e4c1842ee748186a9d2d70d67617ec80ec9a
Salem Shaikh <52129128+slmsshk@users.noreply.github.com>
2023-01-17 15:04:12 +0530

    Update main.py

--- Chatbot/main.py
+++ Chatbot/main.py
@@ -7,11 +7,11 @@ openai.api_key = "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"
 
 prompt = (f"Write a story about an adventure")
 text_input=st.text_input('Enter a prompt')
-
+t=st.slider('slide to set temperature value', min_value=0.1, max_value=1.0)
 completions = openai.Completion.create(
     engine="text-davinci-002",
     prompt=text_input,
-    max_tokens=1024,
+    max_tokens=inf,
     n=1,
     stop=None,
     temperature=0.5,
